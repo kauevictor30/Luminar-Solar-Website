e2b3800885de0ce408649c1f5192016968bdfcc6
Kauê Victor / DEV <kauephbnet@gmail.com>
2025-07-27 12:17:30 -0300

    fix: modifyed buttom redirect to whatsapp, fix padding h1 and p

--- components/organisms/hero-section.tsx
+++ components/organisms/hero-section.tsx
@@ -5,11 +5,11 @@ export function HeroSection() {
   return (
     <section
       id="inicio"
-      className="relative flex items-center justify-center text-center px-4 py-12 sm:py-20 md:py-24 bg-cover bg-center min-h-[80vh]"
+      className="relative flex items-center justify-center text-center px-4 py-12 sm:py-20 md:py-24 bg-cover bg-center min-h-[120vh]"
       style={{ backgroundImage: "url('/hero.png')" }}
     >
       {/* Conteúdo centralizado com sombra no texto */}
-      <div className="z-10 max-w-2xl w-full text-white">
+      <div className="z-10 max-w-2xl w-full text-white -translate-y-20">
         <h1
           className="text-4xl font-bold md:text-5xl lg:text-6xl"
           style={{
@@ -20,7 +20,7 @@ export function HeroSection() {
           Economize com energia solar
         </h1>
         <p
-          className="mt-4 text-lg sm:text-xl"
+          className="mt-4 text-lg sm:text-x"
           style={{
             color: "#001D4A",
             textShadow: "1px 1px 4px rgba(0,0,0,0.6)"
@@ -29,14 +29,14 @@ export function HeroSection() {
           Projetos personalizados para residências, empresas e propriedades rurais.
         </p>
         <div className="mt-6">
-          <a href="#form-section">
+          <a href="https://wa.me/86988114279">
             <Button
               size="lg"
               className="bg-luminar-yellow hover:bg-yellow-600 text-luminar-blue px-6 sm:px-8 py-3 sm:py-4 text-base sm:text-lg font-semibold"
             >
               Fale com um especialista
             </Button>
-          </a>
+          </a>  
         </div>
       </div>
     </section>
